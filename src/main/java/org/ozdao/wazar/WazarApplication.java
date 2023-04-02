8e6fcba8204118de2c929d7cff8e9e91064cb42c
Pavel Lukyanskiy <affklay@gmail.com>
2023-04-02 21:24:29 +0300

    spring boot

---
+++ src/main/java/org/ozdao/wazar/WazarApplication.java
@@ -0,0 +1,13 @@
+package org.ozdao.wazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WazarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ozdao/wazar/WazarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ozdao.wazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WazarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
